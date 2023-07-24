4498eef2096d81255a85a82a3eb97daddc264f34
thecipherninja <dassammy5@gmail.com>
2023-07-24 14:04:52 +0530

    added components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head';
+import { Flex } from '@chakra-ui/react';
+import Header from '../components/Header';
+import List from '../components/List';
+import Map from '../components/Map';
+import Place from '../components/Place';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Travel Advisor</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello World</h1>
-    </>
+      <Flex>
+        <Head>
+          <title>Travel Advisor</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <List />
+        <Map />
+      </Flex>
   );
 };
